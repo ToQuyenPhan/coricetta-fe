77d87b71a6b671af5f57a33b56b733bc412c0139
ToQuyenPhan <duonglh024@gmail.com>
2023-07-03 07:58:23 +0700

    add Search Recipes Page

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,18 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import Header from "../../components/Header";
+import { AiOutlineMenu, AiOutlineSearch, AiOutlineLogout } from 'react-icons/ai'
+import { MdOutlineMenuBook, MdOutlineRestaurantMenu } from 'react-icons/md'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { Popover, Transition } from '@headlessui/react'
+import { BsPersonBoundingBox } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 function Search() {
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
   const token = localStorage.getItem("Token");
   const location = useLocation();
-  const recipeId = location.state?.recipeId;
-  const userId = location.state?.userId;
+  const searchFromHome = location.state?.searchString;
   const navigate = useNavigate();
 
-  const fetchRecipeData = async () => {
+  const fetchRecipeData = async (value) => {
     const res = await fetch(
-      `https://localhost:44327/api/Recipes/all?currentPage=1&pageSize=10`,
+      `https://localhost:44327/api/Recipes/all?currentPage=1&pageSize=8&recipeName=${value}`,
       {
         mode: "cors",
         method: "GET",
@@ -30,6 +35,14 @@ function Search() {
     }
   };
 
+  const handleSearch = () => {
+    fetchRecipeData(search);
+  }
+
+  const handleSearchChange = event => {
+    setSearch(event.target.value);
+  };
+
   const getRecipe = (id) => {
     navigate("/recipeDetails", { state: { recipeId: id } });
   };
@@ -40,17 +53,81 @@ function Search() {
       if (role !== "USER") {
         navigate("/");
       } else {
-        fetchRecipeData();
+        if (searchFromHome !== '') {
+          fetchRecipeData(searchFromHome);
+          setSearch(searchFromHome);
+        }
       }
     }
   }, []);
 
   return (
     <div>
-      <Header />
+      <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4 shadow-md">
+        {/* Left side */}
+        <div className="flex items-center">
+          <div className="cursor-pointer hidden">
+            <AiOutlineMenu size={30} />
+          </div>
+          <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">Co<span className="font-bold">Ricetta</span></h1>
+        </div>
+
+        {/* Search */}
+        <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+          <AiOutlineSearch size={25} onClick={handleSearch} className="hover:cursor-pointer" />
+          <input className="bg-transparent p-2 focus:outline-none w-full" type="text" placeholder="Tìm kiếm công thức" required
+            onChange={handleSearchChange} value={search} minLength={1} maxLength={50} />
+        </div>
+
+        {/* Nav menu */}
+        <div className="flex items-center justify-between p-6">
+          <Popover.Group className="hidden lg:flex lg:gap-x-12">
+            <Link to="/home" className="text-sm font-semibold leading-6 text-gray-900 no-underline">
+              Home
+            </Link>
+            <Popover className="relative">
+              <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+                User
+                <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+              </Popover.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-1"
+              >
+                <Popover.Panel className="absolute right-0 top-full z-10 mt-3 w-screen max-w-[300px] overflow-hidden rounded-xl
+                                 bg-white shadow-lg ring-1 ring-gray-900/5">
+                  <Link to="/profile" className="px-3 py-2 text-center border-b flex gap-3 justify-center items-center">
+                    <BsPersonBoundingBox size={20} />
+                    Profile
+                  </Link>
+                  <Link to="/my-recipes" className="px-3 py-2 text-center border-b flex gap-2 justify-center items-center">
+                    <MdOutlineRestaurantMenu size={20} />
+                    My Recipes
+                  </Link>
+                  <Link to="/my-menus" className="px-3 py-2 border-b flex gap-2 justify-center items-center no-underline
+                                     text-black">
+                    <MdOutlineMenuBook size={20} />
+                    My Menus
+                  </Link>
+                  <Link to="/" className="px-3 py-2 text-center border-b flex gap-3 justify-center items-center 
+                                    no-underline text-black">
+                    <AiOutlineLogout size={20} />
+                    Log out
+                  </Link>
+                </Popover.Panel>
+              </Transition>
+            </Popover>
+          </Popover.Group>
+        </div>
+      </div>
       <div className="mt-5">
         <div>
-          <h1 className="text-orange-600 font-bold text-4xl ml-5 mb-3 inline-block">Danh sách các công thức:</h1>
+          <h1 className="text-orange-600 font-bold text-4xl ml-5 mb-3 inline-block">Danh sách các công thức cho "{search}"</h1>
           <h2 className="inline-block float-right font-bold mr-3">Tổng cộng {recipes.length} công thức</h2>
         </div>
         {recipes?.length > 0 && (
