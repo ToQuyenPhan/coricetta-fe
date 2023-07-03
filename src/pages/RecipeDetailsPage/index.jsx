530b42a5b2b3cbca3e7077b0c74b60150d990928
ToQuyenPhan <duonglh024@gmail.com>
2023-07-03 23:25:43 +0700

    add Report Recipe button

--- src/pages/RecipeDetailsPage/index.jsx
+++ src/pages/RecipeDetailsPage/index.jsx
@@ -1,12 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { BiSolidFlag } from 'react-icons/bi';
 import axios from "axios";
 import Header from "../../components/Header";
 import Comment from "./components/comment";
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+} from "@material-tailwind/react";
+import Swal from 'sweetalert2';
 
 function RecipeDetail() {
   const [recipe, setRecipe] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [description, setDescription] = useState('');
   const token = localStorage.getItem("Token");
+  const userId = localStorage.getItem("Id");
   //const navigate = useNavigate();
   const location = useLocation();
   const recipeId = location.state?.recipeId;
@@ -34,6 +46,12 @@ function RecipeDetail() {
       });
   };
 
+  const handleOpen = () => setOpen(!open);
+
+  const handleDescriptionChange = event => {
+    setDescription(event.target.value);
+  };
+
   useEffect(() => {
     fetchRecipeData();
   }, []);
@@ -56,8 +74,8 @@ function RecipeDetail() {
           Đang tải...
         </div>
       ) : (
-        <section className="section blog-wrap bg-gray">
-          <div className="container">
+        <section className="section blog-wrap bg-gray-300">
+          <div className="container bg-white">
             <div className="row">
               <div className="col-lg-8">
                 <div className="row">
@@ -65,6 +83,42 @@ function RecipeDetail() {
                     <div className="single-blog-item">
                       <div className="blog-item-content p-5">
                         <br />
+                        <div className="w-fit float-right mr-5">
+                          <Fragment className="grid place-items-center">
+                            <Button onClick={handleOpen} variant="gradient" className="shadow-none text-yellow-400 hover:text-red-600 flex justify-center items-center 
+                    hover:cursor-pointer">
+                              <span >Báo cáo</span>
+                              <BiSolidFlag size={20} />
+                            </Button>
+                            <Dialog open={open} handler={handleOpen} className="max-w-[1000px] text-center ">
+
+                              <DialogHeader><h2 className="font-bold text-center w-full text-orange-600">Báo cáo công thức</h2></DialogHeader>
+                              <DialogBody divider>
+                                <form id="report">
+                                  <div className="mb-4">
+                                    <h5 className="text-left ml-3 font-bold">Mô tả:</h5>
+                                    <input className='border border-gray-300 p-3 w-full rounded font-sans text-base text-black focus:outline-0'
+                                      type="text" placeholder="Nhập mô tả về hành vi của công thức!" onChange={handleDescriptionChange} value={description}
+                                      required minLength={10} maxLength={250} />
+                                  </div>
+                                </form>
+                              </DialogBody>
+                              <DialogFooter className="flex justify-end">
+                                <Button
+                                  variant="text"
+                                  color="red"
+                                  onClick={handleOpen}
+                                  className="mr-3"
+                                >
+                                  <span className="text-xl">Hủy bỏ</span>
+                                </Button>
+                                <Button variant="gradient" color="red" type="submit" className=" bg-red-600 px-3 py-1" form="report">
+                                  <span className="text-xl">Báo cáo</span>
+                                </Button>
+                              </DialogFooter>
+                            </Dialog>
+                          </Fragment>
+                        </div>
                         <h3 className="quote">{recipe?.recipeName}</h3>
                         <span className="text-muted mr-3 mb-4">
                           Tác giả: <span className="font-bold">{recipe?.userName}</span>
@@ -117,10 +171,11 @@ function RecipeDetail() {
                 </div>
               </div>
             </div>
+            <Comment />
           </div>
         </section>
       )}
-      <Comment/>
+
       <br />
     </div>
   );
