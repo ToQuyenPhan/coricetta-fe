f05ae12eeef7f53b344fed66c06f9f99f71a50a3
ToQuyenPhan <duonglh024@gmail.com>
2023-07-10 07:10:49 +0700

    add recaptcha

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { useState } from "react";
 import jwtDecode from 'jwt-decode';
 import Swal from 'sweetalert2';
 import { useNavigate, Link } from 'react-router-dom';
+import ReCAPTCHA from "react-google-recaptcha";
 
 function App() {
   const [email, setEmail] = useState('');
   const [password, setPasword] = useState('');
   const [message, setMessage] = useState('');
+  const [verified, setVerified] = useState(false);
   const navigate = useNavigate();
 
   let headers = new Headers();
@@ -16,15 +18,15 @@ function App() {
   let url = "https://localhost:44327/api/Users/login";
 
   useEffect(() => {
-    if(localStorage){
+    if (localStorage) {
       var role = localStorage.getItem('Role');
-      if(role === 'USER'){
+      if (role === 'USER') {
         localStorage.removeItem("Token");
         localStorage.removeItem("Role");
         localStorage.removeItem("Id");
       }
     }
-}, [message])
+  }, [message])
 
   const handleEmailChange = event => {
     setEmail(event.target.value);
@@ -34,9 +36,15 @@ function App() {
     setPasword(event.target.value);
   };
 
+  function onChange(value) {
+    if(value !== undefined){
+      setVerified(true);
+    }
+  };
+
   const fetchUserData = async (e) => {
     e.preventDefault();
-    const res = await fetch(url, { mode: 'cors', method: 'POST', headers: headers, body: JSON.stringify({ "email" : email, "password": password})});
+    const res = await fetch(url, { mode: 'cors', method: 'POST', headers: headers, body: JSON.stringify({ "email": email, "password": password }) });
     if (res.status === 200) {
       const data = await res.text();
       localStorage.setItem("Token", data);
@@ -49,19 +57,19 @@ function App() {
         showConfirmButton: false,
         timer: 1500
       })
-      if(jwtDecode(data).Role === 'USER'){
+      if (jwtDecode(data).Role === 'USER') {
         navigate('/home');
-      }else{
+      } else {
         navigate('/users');
       }
-    }else{
+    } else {
       const data = await res.text();
       setMessage(data);
       Swal.fire({
-          icon: 'error',
-          title: 'Oops...',
-          text: data
-        })
+        icon: 'error',
+        title: 'Oops...',
+        text: data
+      })
     }
   };
 
@@ -70,26 +78,32 @@ function App() {
       <div className="align-items-center absolute w-1/3 left-20 shadow-2xl">
         <div className="p-7 w-full bg-white">
           <div className='text-center'><h1 className='text-black text-4xl mb-5 font-extrabold'>Login</h1></div>
-          <div className='text-center text-black font-bold text-lg mb-3'>More <span className=' text-green-700'>interesting and good</span> recipes in Vietnam!</div> 
+          <div className='text-center text-black font-bold text-lg mb-3'>More <span className=' text-green-700'>interesting and good</span> recipes in Vietnam!</div>
           <div>
             <div className="px-4 py-5">
               <form onSubmit={fetchUserData}>
                 <div className="mb-4">
-                  <input className='border border-gray-300 p-3 w-full rounded-3xl font-sans text-base text-black focus:outline-0' 
-                  type="email" placeholder="Email" required onChange={handleEmailChange} value={email} minLength={6} maxLength={50}/>
+                  <input className='border border-gray-300 p-3 w-full rounded-3xl font-sans text-base text-black focus:outline-0'
+                    type="email" placeholder="Email" required onChange={handleEmailChange} value={email} minLength={6} maxLength={50} />
                 </div>
                 <div className="mb-4">
-                  <input className='border border-gray-300 p-3 w-full rounded-3xl font-sans text-base text-black focus:outline-0' 
-                  type="password" placeholder="Password" required onChange={handlePasswordChange} value={password} minLength={1} maxLength={50}/>
+                  <input className='border border-gray-300 p-3 w-full rounded-3xl font-sans text-base text-black focus:outline-0'
+                    type="password" placeholder="Password" required onChange={handlePasswordChange} value={password} minLength={1} maxLength={50} />
+                </div>
+                <div className='flex justify-center'>
+                  <ReCAPTCHA
+                    sitekey="6LcXX7ceAAAAAPYPH8EzPWO1rZl946Sj6Qgu7xZT"
+                    onChange={onChange}
+                  />
                 </div>
                 <div className="mb-4">
-                  <button className=' bg-gray-700 p-3 w-full rounded-3xl font-sans text-base text-white hover:bg-gray-500' type='submit'>Login</button>
+                  <button className=' bg-gray-700 mt-3 p-3 w-full rounded-3xl font-sans text-base text-white hover:bg-gray-500' type='submit' disabled={!verified}>Login</button>
                 </div>
                 <div className='text-center'>
-                    <p className='text-black inline-block'>
-                        Don't have an account? 
-                        <Link to="/SignUp" className='ml-5 italic underline text-blue-800'>Sign Up</Link>
-                    </p>
+                  <p className='text-black inline-block'>
+                    Don't have an account?
+                    <Link to="/SignUp" className='ml-5 italic underline text-blue-800'>Sign Up</Link>
+                  </p>
                 </div>
               </form>
             </div>
